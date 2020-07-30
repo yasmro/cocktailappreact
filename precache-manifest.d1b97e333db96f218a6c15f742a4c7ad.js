1c73c78585362ead9239c99315af8b55fe207134
Yu Ohno <kantillo0683@gmail.com>
2020-07-31 02:06:40 +0900

    Updates

---
+++ precache-manifest.d1b97e333db96f218a6c15f742a4c7ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0cd6a91be8bd22d27bbb5fb2c56379",
+    "url": "/cocktailappreact/index.html"
+  },
+  {
+    "revision": "101e64f81f692a227a1a",
+    "url": "/cocktailappreact/static/css/main.061ff2ab.chunk.css"
+  },
+  {
+    "revision": "0dc257e0188e671bdcc1",
+    "url": "/cocktailappreact/static/js/2.070a2f01.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/cocktailappreact/static/js/2.070a2f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101e64f81f692a227a1a",
+    "url": "/cocktailappreact/static/js/main.760126da.chunk.js"
+  },
+  {
+    "revision": "ef062d3286336811333c",
+    "url": "/cocktailappreact/static/js/runtime-main.33548846.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cocktailappreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
